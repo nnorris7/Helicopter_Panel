14904e490bc91d4295a5630e4e3714f6547ec786
Norm Norris <norm@ncnenterprises.com>
2019-09-21 12:39:24 -0700

    Initial Commit

---
+++ Helicopter Panel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Helicopter Panel
+//
+//  Created by Norman Norris on 9/21/19.
+//  Copyright © 2019 Norman Norris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
